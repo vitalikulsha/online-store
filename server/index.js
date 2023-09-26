a5af09cfde5e5669ace34c55c5ec9a8b15e81d35
Vitali Kulsha <kulsha_85@mail.ru>
2023-09-26 15:45:05 +0300

    add models

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db');
+const sequelize = require('./db'); //инструмент для организации взоимодействия Node.js и БД
 const PORT = process.env.PORT || 5000;
+const models = require('./models/models');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const start = async () => {
     try {
-        await sequelize.authenticate();
-        await sequelize.sync();
+        await sequelize.authenticate(); //подключение к БД
+        await sequelize.sync(); //сверяет состояние БД со схемой данных
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (e) {
         console.log(e);
